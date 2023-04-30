9481b43fe1000e8140ad4d235100135f28ed8976
mosesluna <mosesluna905@gmail.com>
2023-04-29 21:54:50 -0400

    clone

---
+++ control.py
@@ -0,0 +1,24 @@
+import flashcards.py
+
+def quiz(answer: str, correct: str, score: int):
+    #take in user selected answer, and correct answer
+    #return true or false based on answer
+    choices = []
+    playerChoice = -1
+    
+def learn():
+    #generate a word with definition and show to the user
+    haha = 1
+    
+def resetVariables(choices: list[str], playerChoice: int):
+    choices = []
+    playerChoice = -1
+    
+def newCard():
+    flashcard = ()
+    return flashcard
+    
+def newQuestion():
+    questions = [(), (), (), ()]
+    return questions
+    
